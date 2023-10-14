bb6440714f129077283af41a73dae9fa56575250
mxrkfrxncxs <markyvenus@gmail.com>
2023-10-15 01:37:43 +0800

    another update

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_mysql_connector import MySQL
 from flask_bootstrap import Bootstrap
 from config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY, BOOTSTRAP_SERVE_LOCAL
@@ -20,5 +20,9 @@ def create_app(test_config=None):
     bootstrap.init_app(app)
     mysql.init_app(app)
     CSRFProtect(app)
-
+    
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+    
     return app
